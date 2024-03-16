0bd76bade785d78c017164127db05df260553009
jaymalave <jaym.github@gmail.com>
2024-03-16 14:28:22 +0000

    Ft: mark page ui done

--- src/app/dataset/[id]/page.tsx
+++ src/app/dataset/[id]/page.tsx
@@ -4,30 +4,48 @@ import { useParams } from "next/navigation";
 import { Slider } from "@/components/ui/slider";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import Hero from "@/components/Hero";
+import ConnectWallet from "@/components/ConnectWallet";
 
 export default function DatasetPage(props: any) {
   const { id } = useParams();
   const [score, setScore] = useState(5);
   return (
-    <div className="p-10">
-      <h1>Dataset</h1>
-      <div className="flex justify-between items-start w-90">
-        <div className="flex flex-col items-start justify-start w-100 h-100 mt-10">
-          <h2>Dataset ID: {id}</h2>
-          <p>Name: {props.name}</p>
-          Mark the resume out of 10: {score}
-          <Slider
-            defaultValue={[5]}
-            max={10}
-            step={1}
-            value={[score]}
-            className="w-[300px] my-10"
-            onValueChange={(value) => setScore(value[0])}
-          />
-          <Button variant={"outline"}>Submit</Button>
+    <>
+      <Hero />
+      <ConnectWallet />
+      <div className="p-10 h-screen overflow-hidden">
+        <div className="flex flex-row-reverse justify-between items-start w-90 mt-5">
+          <div className="flex flex-col items-start justify-start w-[30%] h-[90vh] mt-[100px] border-l-1 border-gray-600 px-1">
+            <div className="flex flex-col items-start justify-around h-[30vh]">
+              <h2>
+                Dataset ID: <b>{id}</b>
+              </h2>
+              <p>Name: {props.name}</p>
+              Mark the resume out of 10: {score}
+              <Slider
+                defaultValue={[5]}
+                max={10}
+                step={1}
+                value={[score]}
+                className="w-[300px] my-5"
+                onValueChange={(value) => setScore(value[0])}
+              />
+            </div>
+            <div className="w-full flex flex-col justify-end items-start h-[45vh]">
+              <Button variant={"outline"} className="w-full">
+                Submit
+              </Button>
+              <Button variant={"link"} className="w-full">
+                Exit
+              </Button>
+            </div>
+          </div>
+          <div className="w-[70%] flex items-center justify-center">
+            <iframe src="/a.pdf" className="h-[650px] w-[900px] rounded-sm" />
+          </div>
         </div>
-        <iframe src="/a.pdf" className="h-[600px] w-[500px] rounded-lg" />
       </div>
-    </div>
+    </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Router } from "next/router";
+
 export default function Hero() {
+  const router = useRouter();
   return (
     <div className="fixed top-5 left-10 font-bold">
-      <h1 className="text-2xl">datacat.</h1>
+      <h1
+        className="text-2xl cursor-pointer"
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        datacat.
+      </h1>
     </div>
   );
 }
